3f6100a71674f9e180c3dde290e24f13b37210c9
Alexandre Fonseca <alexandrefonsecach@gmail.com>
2025-07-05 21:04:21 +0200

    fix front api service

--- client/app/api/whoop/connect/route.ts
+++ client/app/api/whoop/connect/route.ts
@@ -18,6 +18,7 @@ export async function POST(req: NextRequest) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
       },
       body: JSON.stringify({
         walletAddress: wallet_address
